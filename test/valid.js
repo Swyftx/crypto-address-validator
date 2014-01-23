24c51b5a32e026599819269c3b1f6d27aeeeea62
Ryan Ralph <ryanralph1991+bitbucket@gmail.com>
2014-01-24 01:55:35 +1100

    Corrected btc tests

--- test/valid.js
+++ test/valid.js
@@ -5,10 +5,10 @@ test('valid', function() {
     function validate(addr, type) {
         assert.ok(address.validate(addr, type));
     };
-
+//----------------BTC ADDRESSES--------------------------------
     validate('1A1zP1eP5QGefi2DMPTfTL5SLmv7DivfNa');
-    validate('1A1zP1eP5QGefi2DMPTfTL5SLmv7DivfNa', 'prod');
-    validate('mzBc4XEFSdzCDcTxAgf6EZXgsZWpztRhef', 'testnet');
+    validate('1A1zP1eP5QGefi2DMPTfTL5SLmv7DivfNa', 'bitcoin');
+    validate('mzBc4XEFSdzCDcTxAgf6EZXgsZWpztRhef', 'bitcoinTestnet');
 
     validate('12KYrjTdVGjFMtaxERSk3gphreJ5US8aUP');
     validate('12QeMLzSrB8XH8FvEzPMVoRxVAzTr5XM2y');
@@ -18,7 +18,7 @@ test('valid', function() {
 
     // p2sh addresses
     validate('3NJZLcZEEYBpxYEUGewU4knsQRn1WM5Fkt');
-    validate('3NJZLcZEEYBpxYEUGewU4knsQRn1WM5Fkt', 'prod');
-    validate('2MxKEf2su6FGAUfCEAHreGFQvEYrfYNHvL7', 'testnet');
+    validate('3NJZLcZEEYBpxYEUGewU4knsQRn1WM5Fkt', 'bitcoin');
+    validate('2MxKEf2su6FGAUfCEAHreGFQvEYrfYNHvL7', 'bitcoinTestnet');
 });
 
