3f1853db5eea6cf1576584ab1db7509809efe40d
Ryan Ralph <ryanralph1991+bitbucket@gmail.com>
2014-01-24 01:50:02 +1100

    Fixed a number of errors, now to write tests

--- index.js
+++ index.js
@@ -7,45 +7,45 @@ var base58 = require('./base58');
 
 var address_types = {
     bitcoin: '00', 		//  0 Decimal 1 prefix
-    bitcoin-testnet: '6f', 	//111 Decimal m-n prefix
+    bitcoinTestnet: '6f', 	//111 Decimal mn prefix
     litecoin: '30', 		// 48 Decimal L prefix
-    litecoin-testnet: '6f' 	//111 Decimal m-n prefix
+    litecoinTestnet: '6f',	//111 Decimal mn prefix
     peercoin: '37', 		// 55 Decimal P prefix
-    peercoin-testnet: '6f' 	//111 Decimal m-n prefix
+    peercoinTestnet: '6f', 	//111 Decimal mn prefix
     dogecoin: '1e', 		// 30 Decimal D prefix
-    dogecoin-testnet: '71' 	//113 Decimal n prefix
+    dogecoinTestnet: '71', 	//113 Decimal n prefix
     freicoin: '00', 		//  0 Decimal 1 prefix
-    freicoin-testnet: '6f'	//111 Decimal m-n prefix
+    freicoinTestnet: '6f',	//111 Decimal mn prefix
     protoshares: '38',		// 56 Decimal P prefix
-    protoshares-testnet: '6f' //111 Decimal m-n prefix
+    protosharesTestnet: '6f', //111 Decimal mn prefix
     megacoin: '32',		// 50 Decimal M prefix
-    megacoin-testnet: '6f',	//111 Decimal m-n prefix
+    megacoinTestnet: '6f',	//111 Decimal mn prefix
     primecoin: '17', 		// 23 Decimal A prefix
-    primecoin-testnet: '6f',	//111 Decimal m-n prefix
-    feathercoin: '0E' 		// 14 Decimal 5-6 prefix
-    feathercoin-testnet: '6f' //111 Decimal m-n prefix
+    primecoinTestnet: '6f',	//111 Decimal mn prefix
+    feathercoin: '0E', 		// 14 Decimal 56 prefix
+    feathercoinTestnet: '6f' //111 Decimal mn prefix
     //That's all for now, to add more just send a pull request
 };
 
 var p2sh_types = {
     bitcoin: '05', 		//  5 Decimal 3 prefix
-    bitcoin-testnet: 'c4', 	//196 Decimal 2 prefix
+    bitcoinTestnet: 'c4', 	//196 Decimal 2 prefix
     litecoin: '05',		//  5 Decimal 3 prefix
-    litecoin-testnet: 'c4',	//196 Decimal 2 prefix
+    litecoinTestnet: 'c4',	//196 Decimal 2 prefix
     peercoin: '75',		//117 Decimal p prefix
-    peercoin-testnet: 'c4',	//196 Decimal 2 prefix
-    dogecoin: '16',		//22 Decimal 9-A prefix
-    dogecoin-testnet: 'c4',	//196 Decimal 2 prefix
+    peercoinTestnet: 'c4',	//196 Decimal 2 prefix
+    dogecoin: '16',		//22 Decimal 9A prefix
+    dogecoinTestnet: 'c4',	//196 Decimal 2 prefix
     freicoin: '05',		//  5 Decimal 3 prefix
-    freicoin-testnet: 'c4', 	//196 Decimal 2 prefix
+    freicoinTestnet: 'c4', 	//196 Decimal 2 prefix
     protoshares: '05',		//  5 Decimal 3 prefix
-    protoshares-testnet: 'c4',//196 Decimal 2 prefix
+    protosharesTestnet: 'c4',//196 Decimal 2 prefix
     megacoin: '05',		//  5 Decimal 3 prefix
-    megacoin-testnet: 'c4', 	//196 Decimal 2 prefix
+    megacoinTestnet: 'c4', 	//196 Decimal 2 prefix
     primecoin: '53',		// 83 Decimal a prefix
-    primecoin-testnet: 'c4',  //196 Decimal 2 prefix
+    primecoinTestnet: 'c4',  //196 Decimal 2 prefix
     feathercoin: '05',		//  5 Decimal 3 prefix
-    feathercoin-testnet: 'c4' //196 Decimal 2 prefix
+    feathercoinTestnet: 'c4' //196 Decimal 2 prefix
 };
 
 /// return address type if valid base58 address, otherwise null
@@ -81,7 +81,7 @@ module.exports.get_address_type = get_address_type;
 /// return {boolean} true if valid, false otherwise
 function validate(address, address_type) {
     // default is to check that address is regular production address
-    address_type = address_type || 'btc';
+    address_type = address_type || 'bitcoin';
 
     var type = get_address_type(address);
     if (type === null) {
