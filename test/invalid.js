183549a9fac4ad71ca9201e168488773ce056a92
Ryan Ralph <ryanralph1991+bitbucket@gmail.com>
2014-01-24 02:07:11 +1100

    list worked?

--- test/invalid.js
+++ test/invalid.js
@@ -5,20 +5,20 @@ test('invalid', function() {
     function invalid(addr, type) {
         assert.ok(!address.validate(addr, type));
     };
-
+//-----------------------BTC ADDRESSES-------------------------------
     invalid('');
     invalid('mzBc4XEFSdzCDcTxAgf6EZXgsZWpztRhe');
-    invalid('mzBc4XEFSdzCDcTxAgf6EZXgsZWpztRhef', 'prod');
+    invalid('mzBc4XEFSdzCDcTxAgf6EZXgsZWpztRhef', 'bitcoin');
 
     invalid('bd839e4f6fadb293ba580df5dea7814399989983');
     invalid('miCVC7QcY917Cz427qTBEUrvBzRapHrupc');
     invalid('rrRmhfXzGBKbV4YHtbpxfA1ftEcry8AJaX');
 
     // reject litecoin addresses
-    invalid('LSxNsEQekEpXMS4B7tUYstMEdMyH321ZQ1', 'prod');
+    invalid('LSxNsEQekEpXMS4B7tUYstMEdMyH321ZQ1', 'bitcoinTestnet');
 
     // testnet
-    invalid('1A1zP1eP5QGefi2DMPTfTL5SLmv7DivfNa', 'testnet');
+    invalid('1A1zP1eP5QGefi2DMPTfTL5SLmv7DivfNa', 'bitcoinTestnet');
 
     // invalid base58 string
     invalid('%%@');
