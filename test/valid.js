3db865a1e4718ee77a0d043507b9957c19a3c0ef
Ryan Ralph <ryan@ryanralph.net>
2014-03-04 15:01:02 +1100

    Added tests for auroracoin

--- test/valid.js
+++ test/valid.js
@@ -23,7 +23,7 @@ test('valid', function() {
     validate('LVg2kJoFNg45Nbpy53h7Fe1wKyeXVRhMH9', 'litecoin');
     validate('LTpYZG19YmfvY2bBDYtCKpunVRw7nVgRHW', 'litecoin');
     validate('Lb6wDP2kHGyWC7vrZuZAgV7V4ECyDdH7a6', 'litecoin');
-    validate('mzBc4XEFSdzCDcTxAgf6EZXgsZWpztRhef', 'litecoinTestnet'); 
+    validate('mzBc4XEFSdzCDcTxAgf6EZXgsZWpztRhef', 'litecoinTestnet');
 
     // p2sh addresses
     validate('3NJZLcZEEYBpxYEUGewU4knsQRn1WM5Fkt', 'litecoin');
@@ -34,7 +34,7 @@ test('valid', function() {
     validate('PSbM1pGoE9dnAuVWvpQqTTYVpKZU41dNAz', 'peercoin');
     validate('PUULeHrJL2WujJkorc2RsUAR3SardKUauu', 'peercoin');
     validate('mzBc4XEFSdzCDcTxAgf6EZXgsZWpztRhef', 'peercoinTestnet');
-    
+
     // p2sh addresses
     validate('pNms4CaWqgZUxbNZaA1yP2gPr3BYnez9EM', 'peercoin');
     validate('2MxKEf2su6FGAUfCEAHreGFQvEYrfYNHvL7', 'peercoinTestnet');
@@ -58,17 +58,17 @@ test('valid', function() {
     // p2sh addresse
     validate('3NJZLcZEEYBpxYEUGewU4knsQRn1WM5Fkt', 'freicoin');
     validate('2MxKEf2su6FGAUfCEAHreGFQvEYrfYNHvL7', 'freicoinTestnet');
-    
+
 //----------------PTS ADDRESSES--------------------------------
     validate('PaNGELmZgzRQCKeEKM6ifgTqNkC4ceiAWw', 'protoshares');
     validate('Piev8TMX2fT5mFtgxx2TXJaqXP37weMPuD', 'protoshares');
     validate('PgsuLoe9ojRKFGJGVpqqk37gAqNJ4ozboD', 'protoshares');
     validate('mzBc4XEFSdzCDcTxAgf6EZXgsZWpztRhef', 'protosharesTestnet');
-    
+
     //p2sh addresses
     validate('3NJZLcZEEYBpxYEUGewU4knsQRn1WM5Fkt', 'protoshares');
     validate('2MxKEf2su6FGAUfCEAHreGFQvEYrfYNHvL7', 'protosharesTestnet');
-    
+
 //----------------MEC ADDRESSES--------------------------------
     validate('MWUHaNxjXGZUYTh92i3zuDmsnH1rHSBk5M', 'megacoin');
     validate('MSAkrhRyte7bz999Ga5SqYjzypFFYa2oEb', 'megacoin');
@@ -78,15 +78,25 @@ test('valid', function() {
     //p2sh addresses
     validate('3NJZLcZEEYBpxYEUGewU4knsQRn1WM5Fkt', 'megacoin');
     validate('2MxKEf2su6FGAUfCEAHreGFQvEYrfYNHvL7', 'megacoinTestnet');
-    
+
 //----------------XPM ADDRESSES--------------------------------
     validate('AVKeiZ5JadfWdH2EYVgVRfX4ufoyd4ehuM', 'primecoin');
     validate('AQXBRPyob4dywUJ21RUKrR1xetQCDVenKD', 'primecoin');
     validate('ANHfTZnskKqaBU7oZuSha9SpbHU3YBfeKf', 'primecoin');
     validate('AYdiYMKSGYxLcZNDmqB8jNcck7SQibrfiK', 'primecoin');
     validate('mzBc4XEFSdzCDcTxAgf6EZXgsZWpztRhef', 'primecoinTestnet');
-    
+
     //p2sh addresses
     validate('af5CvTQq7agDh717Wszb5QDbWb7nT2mukP', 'primecoin');
     validate('2MxKEf2su6FGAUfCEAHreGFQvEYrfYNHvL7', 'primecoinTestnet');
+//----------------AUR ADDRESSES---------------------------------
+    validate('ARM3GLZXF1PDTZ5vz3wh5MVahbK9BHTWAN', 'auroracoin');
+    validate('AUtfc6ThCLb7FuEu7QPrWpJuaXaJRPciDF', 'auroracoin');
+    validate('AUN1oaj5hjispGnczt8Aruw3TxgGyRqB3V', 'auroracoin');
+    validate('AXGcBkGX6NiaDXj85C5dCrhTRUgwxSkKDK', 'auroracoin');
+    validate('mzBc4XEFSdzCDcTxAgf6EZXgsZWpztRhef', 'auroracoinTestnet');
+
+    //p2sh addresses
+    validate('af5CvTQq7agDh717Wszb5QDbWb7nT2mukP', 'auroracoin');
+    validate('2MxKEf2su6FGAUfCEAHreGFQvEYrfYNHvL7', 'auroracoinTestnet');
 });
