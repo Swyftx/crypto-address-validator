7e4674b431caf320b4b5fc7455fe7d9856e0cfe5
Tomek Kolasa <kolasa.tomek@gmail.com>
2018-03-26 14:29:05 +0900

    Added .editorconfig and fixed whitespaces

--- src/ripple_validator.js
+++ src/ripple_validator.js
@@ -3,20 +3,20 @@ var baseX = require('base-x');
 var codec = baseX("rpshnaf39wBUDNEGHJKLM4PQRST7VWXYZ2bcdeCg65jkm8oFqi1tuvAxyz"); // for ripple
 
 module.exports = {
-	/**
-	 * ripple address validation
-	 */
-	isValidAddress: function (address) {
-		if (/^r[rpshnaf39wBUDNEGHJKLM4PQRST7VWXYZ2bcdeCg65jkm8oFqi1tuvAxyz]{27,35}$/.test(address) === false)
-			return false;
-		return this.verifyChecksum(address);
-	},
+    /**
+     * ripple address validation
+     */
+    isValidAddress: function (address) {
+        if (/^r[rpshnaf39wBUDNEGHJKLM4PQRST7VWXYZ2bcdeCg65jkm8oFqi1tuvAxyz]{27,35}$/.test(address) === false)
+            return false;
+        return this.verifyChecksum(address);
+    },
 
-	verifyChecksum: function (address) {
-		var bytes = codec.decode(address);
-		var computedChecksum = cryptoUtils.sha256Checksum(cryptoUtils.toHex(bytes.slice(0, -4)));
-		var checksum = cryptoUtils.toHex(bytes.slice(-4));
+    verifyChecksum: function (address) {
+        var bytes = codec.decode(address);
+        var computedChecksum = cryptoUtils.sha256Checksum(cryptoUtils.toHex(bytes.slice(0, -4)));
+        var checksum = cryptoUtils.toHex(bytes.slice(-4));
 
-		return computedChecksum === checksum
-	}
+        return computedChecksum === checksum
+    }
 };
