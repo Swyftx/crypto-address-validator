cebeef61694e001e3e5821f33e88b215d2ed76ea
GusGold <me@angusgoldman.com>
2019-02-07 10:10:41 +1000

    Formatted eth validator

--- src/ethereum_validator.js
+++ src/ethereum_validator.js
@@ -23,8 +23,10 @@ module.exports = {
 
     for (var i = 0; i < 40; i++) {
       // The nth letter should be uppercase if the nth digit of casemap is 1
-      if ((parseInt(addressHash[i], 16) > 7 && address[i].toUpperCase() !== address[i]) ||
-                (parseInt(addressHash[i], 16) <= 7 && address[i].toLowerCase() !== address[i])) {
+      if (
+        (parseInt(addressHash[i], 16) > 7 && address[i].toUpperCase() !== address[i]) ||
+        (parseInt(addressHash[i], 16) <= 7 && address[i].toLowerCase() !== address[i])
+      ) {
         return false
       }
     }
