f6bbb8f944c5f25b92fe72231fe45c7259fe24e5
GusGold <me@angusgoldman.com>
2019-07-15 14:31:10 +1000

    Switch USDT to erc20

--- test/wallet_address_validator.js
+++ test/wallet_address_validator.js
@@ -264,6 +264,7 @@ describe('WAValidator.validate()', function () {
       valid('0xD1220A0cf47c7B9Be7A2E6BA89F429762e7b9aDb', 'ETZ')
       valid('0xD1220A0cf47c7B9Be7A2E6BA89F429762e7b9aDb', 'callisto')
       valid('0xD1220A0cf47c7B9Be7A2E6BA89F429762e7b9aDb', 'CLO')
+      valid('0xD1220A0cf47c7B9Be7A2E6BA89F429762e7b9aDb', 'USDT')
     })
 
     it('should return true for correct Ripple addresses', function () {
