ec5b38aefbea6da4e407ad0364d7ad46931f3b07
GusGold <me@angusgoldman.com.au>
2019-09-02 23:29:55 +1000

    Add Mana, IOST, NXS. Add ZEN alias, Horizen

--- debug/index.js
+++ debug/index.js
@@ -1,3 +1,3 @@
 const Validator = require('../src/wallet_address_validator.js')
-console.log(Validator.validate('8BMsVKsZ6kdUVWi4o67iWTe3ZaGL4Qc7KjfLVcPer4v2jNTKfEoXtJoL48EHt8cqdZWBQkjzttygpHJyhZcnqLb1JTfVkim', 'xmr'))
-console.log(Validator.validate('45HqZMyS2VmKzD4V17U7fT2HZj9ZgYgBXJsAJHkjj8QFVuhCxRSxsJB8HrZmGVyFr8HEsDxbKhFCyVLiauFtiPknJeDFAMj', 'xmr'))
+console.log(Validator.validate('binanceiost', 'IOST'))
+console.log(Validator.validate('binanceAost', 'IOST'))
